db8ede4c1566c891f7a2f6ac673c8e347a071d7b
lucianoteseo <lucianoescalante614@gmail.com>
2026-03-12 01:54:59 -0300

    connect frontend with deployed backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: import.meta.env.VITE_API_URL })
+const api = axios.create({ baseURL: "https://taskmanager-api-tjqj.onrender.com" })
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
