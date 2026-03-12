387a53676269d2f7bb12730f7a51e999a37ffb0a
lucianoteseo <lucianoescalante614@gmail.com>
2026-03-12 02:18:30 -0300

    fix api route

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,7 @@
-import axios from 'axios'
+import axios from "axios";
 
-const api = axios.create({ baseURL: "https://taskmanager-api-tjqj.onrender.com" })
+const api = axios.create({
+  baseURL: "https://taskmanager-api-tjqj.onrender.com/api"
+});
 
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('token')
-  if (token) config.headers.Authorization = 'Bearer ' + token
-  return config
-})
-
-export default api
+export default api;
